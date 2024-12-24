f38586d718b617e7152e2f5d8d5b194b0ffb8735
Akash Sapkal <akashsapkal7620@gmail.com>
2024-12-24 11:29:50 +0530

    add theme change feature and local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
 import "./App.css";
 import { TaskList } from "./components/TaskList";
 
 function App() {
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState(JSON.parse(localStorage.getItem("tasklist"))||[]);
   const [editTask, setEditTask] = useState({});
-  
+
+  useEffect(()=>{
+  localStorage.setItem("tasklist",JSON.stringify(taskList));
+  },[taskList])
 
   // document.documentElement.classList.
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Header.css";
 export const Header = () => {
-  const [border, setBorder] = useState("black");
-  const handlelight=()=>{
-    setBorder("black");
-    document.documentElement.classList.add("light");
-    document.documentElement.classList.remove("dark");
-    // console.log(theme);
-  }
-  const handledark=()=>{
-    setBorder("white");
-    document.documentElement.classList.add("dark");
-    document.documentElement.classList.remove("light");
-    // console.log(theme);
-  }
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem("theme");
+    return storedTheme ? JSON.parse(storedTheme) : true;
+  });
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    theme
+      ? document.documentElement.classList.remove("dark")
+      : document.documentElement.classList.add("dark");
+  }, [theme]);
+  const handleTheme = () => {
+    setTheme(!theme);
+  };
   return (
     <header>
       <nav>
@@ -21,16 +21,19 @@ export const Header = () => {
           <i className="bi bi-check2-square"></i>
           <span className="title">TaskMate</span>
         </div>
-        <div className="theme">
-          <span className={`span ${border}`} onClick={handlelight}>
-            <i className="bi bi-brightness-low-fill"></i>
-            <span>light</span>
-          </span>
-          <span className="partition">|</span>
-          <span className={`span ${border}`} onClick={handledark}>
-            <i className="bi bi-moon-stars-fill"></i>
-            <span>dark</span>
-          </span>
+        <div className="theme-div" onClick={handleTheme}>
+          <div className="light">
+            <span>
+              <i className="bi bi-brightness-low-fill"></i>
+              <span>light</span>
+            </span>
+          </div>
+          <div className="dark">
+            <span>
+              <i className="bi bi-moon-stars-fill"></i>
+              <span>dark</span>
+            </span>
+          </div>
         </div>
       </nav>
     </header>
